2e55da39f80dc4b269215428f1011730f6d7ac93
catfishbrorther <43174399+catfishbrorther@users.noreply.github.com>
2019-02-19 20:50:15 +0800

    Add files via upload

---
+++ 207410332/w01-1/app.js
@@ -0,0 +1,30 @@
+console.log('Hello World');
+console.log(123); //207410332
+console.log(true);
+
+var greeting='chung'; //張皓鈞 
+console.log(greeting);
+console.log(1,2,3)//2019/02/19
+console.log({firstname:'chung',lastname:'hao jun'}); // object
+console.table({firstname:'chung',lastname:'hao jun'});
+
+console.error('This is some error');
+console.warn('This is some warning');
+
+console.time('Hello');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd('Hello');
+
+//console.clear();
+/*
+ multi
+ line
+ comment
+*/ 
+
---
+++ 207410332/w01-2/app.js
@@ -0,0 +1,49 @@
+// var,let,const
+
+var name='chung hao jun'
+console.log(name);
+
+name='張皓鈞';
+console.log(name);
+
+name='2019';
+console.log(name);
+
+var greeting;
+console.log(greeting);
+greeting='Hello';
+console.log('Hello');
+
+// letter,number,_,$
+// annot start with number
+
+//Multi word vars
+var firstName='hao jun';//Camel case, Preferred
+var first_name='Yu'; //Underscore
+var firstname='jin'; //Pascal case
+var firstname;
+
+//let
+
+let name2='chang hao jun';
+console.log(name2);
+
+name2='張皓鈞';
+console.log(name2);
+
+name2='2019';
+console.log(name2);
+
+//const
+
+const person={
+    name: 'hjc' ,
+    age: 18 
+};
+person.name='hjchang';
+person.age=18;
+console.log(person);
+
+const numbers=[1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
